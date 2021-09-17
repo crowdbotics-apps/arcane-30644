89e1be9ef037e6a48c404371b7848d3089dc942e
Crowdbotics <team@crowdbotics.com>
2021-09-17 17:01:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "arcane_30644Identifier",
+  url: "https://arcane_30644Identifier.botics.co",
+  api: "https://arcane_30644Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
